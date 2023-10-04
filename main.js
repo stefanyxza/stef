ceeb9d07725a63fb1b74b386068747de9023cae8
stefany <126577667+stefanyxza@users.noreply.github.com>
2023-10-04 19:38:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,53 @@
+function tocarpom () {
+    document.querySelector('#som_tecla_pom' ).play()
+}
+
+document.querySelector('.tecla_pom').onclick = tocarpom;
+ 
+function tocarclap () {
+    document.querySelector('#som_tecla_clap' ).play()
+}
+
+document.querySelector('.tecla_clap').onclick = tocarclap;
+
+function tocartim () {
+    document.querySelector('#som_tecla_tim' ).play()
+}
+
+document.querySelector('.tecla_tim').onclick = tocartim;
+
+function tocarpuff () {
+    document.querySelector('#som_tecla_puff' ).play()
+}
+
+document.querySelector('.tecla_puff').onclick = tocarpuff;
+
+function tocarsplash () {
+    document.querySelector('#som_tecla_splash' ).play()
+}
+
+document.querySelector('.tecla_splash').onclick = tocarsplash;
+
+function tocartoim () {
+    document.querySelector('#som_tecla_toim' ).play()
+}
+
+document.querySelector('.tecla_toim').onclick = tocartoim;
+
+function tocarpsh () {
+    document.querySelector('#som_tecla_psh' ).play()
+}
+
+document.querySelector('.tecla_psh').onclick = tocarpsh;
+
+function tocartic () {
+    document.querySelector('#som_tecla_tic' ).play()
+}
+
+document.querySelector('.tecla_tic').onclick = tocartic;
+
+function tocartom () {
+    document.querySelector('#som_tecla_tom' ).play()
+}
+
+document.querySelector('.tecla_tom').onclick = tocartom;
